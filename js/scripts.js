66ddec69191508c512b5cb8acb52f56a8e451f66
BenOkwara <maranathabenson@gmail.com>
2018-09-25 09:16:50 +0300

    update the jquery function in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function) {
-  
+
+  $("form#fwaweru").submit(function(event){
+  var 1 = $("input:checkbox[name=blockElements]:checked").val();
+  var 2 = $("input:checkbox[name=block]:checked").val();
+  var 3 = $("input:checkbox[name=inlineElements]:checked").val();
+  var 4 = $("input:checkbox[name=inline]:checked").val();
+  var 5 = $("input:checkbox[name=github]:checked").val();
+  var 6 = $("input:checkbox[name=type]:checked").val();
 }
