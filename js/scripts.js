b4c971702579db091d4ef6a1d631eab79d5fdce7
BenOkwara <maranathabenson@gmail.com>
2018-09-26 09:40:07 +0300

    Add the changes on variables in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
 $(document).ready(function() {
 
   $("form#fwaweru").submit(function(event){
-  var Qn1 = $("input:radio[name=blockElements]:checked").val();
-  var Qn2 = $("input:radio[name=block]:checked").val();
-  var Qn3 = $("input:radio[name=inlineElements]:checked").val();
-  var Qn4 = $("input:radio[name=inline]:checked").val();
-  var Qn5 = $("input:radio[name=github]:checked").val();
-  var Qn6 = $("input:radio[name=type]:checked").val();
+  var qN1 = $("input:radio[name=blockElements]:checked").val();
+  var qN2 = $("input:radio[name=block]:checked").val();
+  var qN3 = $("input:radio[name=inlineElements]:checked").val();
+  var qN4 = $("input:radio[name=inline]:checked").val();
+  var qN5 = $("input:radio[name=github]:checked").val();
+  var qN6 = $("input:radio[name=type]:checked").val();
 
-  var youScore = parseInt(Qn1)+parseInt(Qn2)+parseInt(Qn3)+parseInt(Qn4)+parseInt(Qn5)+parseInt(Qn6);
+  var youScore = parseInt(qN1)+parseInt(qN2)+parseInt(qN3)+parseInt(qN4)+parseInt(qN5)+parseInt(qN6);
   $("#youScore").text("HI THERE! HERE'S WHAT YOU SCORED: " +youScore + "%");
 
       //==== user Interface ====//
